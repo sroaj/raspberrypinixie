74d3bb4771dac1fda6fe92aee5d1ad02d7124dc2
Sroaj Sosothikul <sroaj@sosothikul.com>
2017-04-12 23:15:49 -0700

    Added python version supported.
    The raspberrypinixie module should work with all
    pyhton versions.

--- setup.py
+++ setup.py
@@ -17,5 +17,19 @@ setup(
     author='Sroaj Sosothikul',
     url='https://github.com/sroaj/raspberrypinixie',
     license=license,
-    packages=find_packages(exclude=('tests', 'docs', 'samples', 'ext'))
+    packages=find_packages(exclude=('tests', 'docs', 'samples', 'ext')),
+    classifiers=(
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ),
 )
